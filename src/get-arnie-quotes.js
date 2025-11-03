fc416a9f237758c7a61602f23a129eabfa7d7494
beauwilliams <beau.williams@icloud.com>
2025-11-04 09:46:09 +1100

    feat: impl get arnie quotes

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,21 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  return await Promise.all(
+    urls.map(async (url) => {
+      try {
+        const res = await httpGet(url);
+        const data = JSON.parse(res.body);
+        if (res.status === 200) {
+          return { "Arnie Quote": data.message };
+        } else {
+          return { FAILURE: data.message };
+        }
+      } catch (error) {
+        return { FAILURE: error.message };
+      }
+    })
+  );
 };
 
 module.exports = {
